a3c4b35bcff9b2eaa7d7615ace94c37a0b9e89dc
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2025-01-31 11:17:10 +0000

    coccobello1

--- album-animali/app.js
+++ album-animali/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+const animals = require("animals.json");
 app.get('/', function (req, res) {
 res.send('Hello World!');
 });
+app.get('/api/album-animali', function (req, res) {
+    res.send(animals);
+    });
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
 });
